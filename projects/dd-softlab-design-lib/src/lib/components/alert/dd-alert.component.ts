efc6e4fe17cd4a2c55be000319cdefd8d385b61b
dan4o99 <daniel.dilov.arb@gmail.com>
2026-02-20 12:52:52 +0200

    fix(test): add random comment to test PRs 2

--- projects/dd-softlab-design-lib/src/lib/components/alert/dd-alert.component.ts
+++ projects/dd-softlab-design-lib/src/lib/components/alert/dd-alert.component.ts
@@ -7,7 +7,7 @@ import {
 } from "@angular/core";
 import { DdDynamicStyleService } from "../../theming/dynamic-style.service";
 import { DD_ALERT_CSS, DdAlertVariant } from "./dd-alert.style";
-//comment to remove
+//comment to
 @Component({
   selector: "dd-alert",
   changeDetection: ChangeDetectionStrategy.OnPush,
